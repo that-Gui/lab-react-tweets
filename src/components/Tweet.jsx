f99e8a159754d056d82eae4f732ab6b7aca5300a
thatGui <gui@thatgui.com>
2022-02-28 17:00:50 +0000

    submit commit

---
+++ src/components/Tweet.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ProfileImage from "./ProfileImage/ProfileImage";
+import User from "./User/User";
+import Timestamp from "./Timestamp/Timestamp";
+import Message from "./Message/Message";
+import Actions from "./Actions/Actions";
+
+function Tweet(props) {
+  return (
+    <div className="tweet">
+      <ProfileImage image={props.tweet.user.image} />
+
+      <div className="body">
+        
+        <div className="top">
+          <User userData={props.tweet.user} />
+          <Timestamp time={props.tweet.timestamp} />
+        </div>
+
+        <Message message={props.tweet.message} />
+        <Actions />
+        
+      </div>
+
+      <i class="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
+
+export default Tweet;
